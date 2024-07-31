cfe6ea64e15054328a4fc631038782f0273d87bf
aisl1n <aislanarchy@gmail.com>
2024-07-30 23:01:11 -0300

    fix:deleteProducts

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createProduct, getProducts } from "../controllers/ProductController.js";
+import { createProduct, deleteProduct, getProducts } from "../controllers/ProductController.js";
 
 const router = express.Router()
 
 router.post('/', createProduct);
 router.get('/', getProducts);
+router.delete('/:id', deleteProduct);
 
 export default router;
