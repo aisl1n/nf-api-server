f093daf4d2703048e7a53958ded858b150c0675e
aisl1n <aislanarchy@gmail.com>
2024-07-30 22:03:16 -0300

    Ajustado mapeamento cheerio da quantity

--- src/controllers/ProxyController.js
+++ src/controllers/ProxyController.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cheerio from "cheerio";
 
 export const proxyRequest = async (req, res) => {
-  const url = req.query.url
+  const url = req.query.url;
   console.log(url);
   try {
     const response = await axios.get(url);
@@ -12,7 +12,7 @@ export const proxyRequest = async (req, res) => {
 
     $("table tbody tr").each((index, element) => {
       const name = $(element).find("span.txtTit").text().trim();
-      const quantity = $(element).find(".Rqtd").text().trim();
+      const quantity = $(element).find(".Rqtd").clone().children().remove().end().text().trim();
       const price = $(element).find(".txtTit .valor").text().trim();
 
       if (name && quantity && price) {
