0eecafd2e91f0bfaa90a0d9679008f3e870ca0f3
aisl1n <aislanarchy@gmail.com>
2024-08-01 23:26:34 -0300

    feat:purchase, GET by ID, Delete

--- src/controllers/PurchaseController.js
+++ src/controllers/PurchaseController.js
@@ -13,7 +13,7 @@ export const createPurchase = async (req, res) => {
     await purchase.save();
     res.status(201).json(purchase);
   } catch (error) {
-    console.log(error.message)
+    console.log(error.message);
     res.status(500).json({ message: error.message });
   }
 };
@@ -22,6 +22,29 @@ export const getPurchases = async (req, res) => {
   try {
     const purchases = await Purchase.find().populate("products");
     res.status(200).json(purchases);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getPurchaseById = async (req, res) => {
+  try {
+    const purchaseId = req.params.id;
+    const purchase = await Purchase.findById(purchaseId).populate("products");
+    if (!purchase) {
+      return res.status(404).json({ message: "Compra não encontrada" });
+    }
+    res.status(200).json(purchase);
+  } catch (error) {
+    res.status(500).json({ message: "Compra não encontrada" });
+  }
+};
+
+export const deletePurchase = async (req, res) => {
+  try {
+    const purchaseId = req.params.id;
+    await Purchase.findByIdAndDelete(purchaseId);
+    res.status(200).json({ message: "Compra deletada com sucesso" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- src/routes/purchaseRoutes.js
+++ src/routes/purchaseRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { createPurchase, getPurchases } from '../controllers/PurchaseController.js';
+import { createPurchase, deletePurchase, getPurchaseById, getPurchases } from '../controllers/PurchaseController.js';
 
 const router = express.Router();
 
 router.post('/', createPurchase);
 router.get('/', getPurchases);
+router.get('/:id', getPurchaseById);
+router.delete('/:id', deletePurchase);
 
 export default router;
